0cd65e31c73e42b78d10ebc354387ac8c469ff22
Vincent Coetzee <vincent.coetzee@hellogroup.co.za>
2017-06-29 14:57:11 +0200

    Initial Commit

---
+++ UIValueModels/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIValueModels
+//
+//  Created by Vincent Coetzee on 2017/06/29.
+//  Copyright © 2017 MacSemantics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
